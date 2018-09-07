725dc407a3d485d37e1438f073fef3c31eae63fd
Martin Vindis <martin.vindis.80@gmail.com>
2018-09-08 00:31:26 +0200

    First commit

--- AMSCamTool/Form1.Designer.cs
+++ AMSCamTool/Form1.Designer.cs
@@ -28,13 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxPitch = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxRoll = new System.Windows.Forms.TextBox();
+            this.textBoxYaw = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxPitch
+            // 
+            this.textBoxPitch.Location = new System.Drawing.Point(76, 24);
+            this.textBoxPitch.Name = "textBoxPitch";
+            this.textBoxPitch.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPitch.TabIndex = 0;
+            this.textBoxPitch.Text = "0.00000";
+            this.textBoxPitch.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(76, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 21);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxRoll
+            // 
+            this.textBoxRoll.Location = new System.Drawing.Point(76, 76);
+            this.textBoxRoll.Name = "textBoxRoll";
+            this.textBoxRoll.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRoll.TabIndex = 2;
+            this.textBoxRoll.Text = "0.00000";
+            this.textBoxRoll.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxYaw
+            // 
+            this.textBoxYaw.Location = new System.Drawing.Point(76, 50);
+            this.textBoxYaw.Name = "textBoxYaw";
+            this.textBoxYaw.Size = new System.Drawing.Size(100, 20);
+            this.textBoxYaw.TabIndex = 3;
+            this.textBoxYaw.Text = "0.00000";
+            this.textBoxYaw.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Pitch";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Yaw";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Roll";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(226, 145);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxYaw);
+            this.Controls.Add(this.textBoxRoll);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxPitch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AMS CamTool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxPitch;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxRoll;
+        private System.Windows.Forms.TextBox textBoxYaw;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- AMSCamTool/Form1.cs
+++ AMSCamTool/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,31 @@ namespace AMSCamTool
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        string dirPath = Application.StartupPath + "\\GameData\\Vehicles\\";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var file in Directory.GetFiles(dirPath, "*.cam", SearchOption.AllDirectories))
+            {
+                string fileContent = File.ReadAllText(file);
+                //Regex r = new Regex(@"^(LocalCam=TVCOCKPIT[\s\S]*)(OrientationOffset.*)");
+                Regex r = new Regex(@"(LocalCam=COCKPIT[\s{\w=()\.,-]*)(OrientationOffset=\([^)]*\))");
+                Match m = r.Match(fileContent);
+
+                if (m.Success)
+                {
+                    fileContent = r.Replace(fileContent, m.Groups[1] + "OrientationOffset=(" + textBoxPitch.Text + ", " + textBoxYaw.Text + ", " + textBoxRoll.Text + ")");
+                }
+
+                File.WriteAllText(file, fileContent);
+            }
         }
     }
 }
